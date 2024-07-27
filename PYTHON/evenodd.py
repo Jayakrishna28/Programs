4b097a97b3e1e257fbfa3905a40fde0834eb11fe
Jayakrishna28 <jayakrishnakambala3026@gmail.com>
2024-07-27 19:52:49 +0530

    data

--- PYTHON/evenodd.py
+++ PYTHON/evenodd.py
@@ -1,5 +1,5 @@
 num=int(input("Enter a Number"))
 if num % 2 == 0:
-    print("It is a Even Number")
+    print("It is a Eveeennn")
 else:
-     print("It is a Odd Number")
+     print("It is a Odd")

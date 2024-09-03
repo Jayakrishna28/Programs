a175b57bc8cf4cea5302cc8967c4e2e829577865
Jayakrishna28 <jayakrishnakambala3026@gmail.com>
2024-09-03 19:46:48 +0530

    some

--- JAVAPROGRAM/Jayaaa.java
+++ JAVAPROGRAM/Jayaaa.java
@@ -2,9 +2,9 @@ public class Jayaaa {
     public static void main(String[] args) {
         if(2<3){
             System.out.println("True");
-            else(){
+        else() {
                System.out.println("False");
-        }
+               }
         }
     }
 }
